9695169860198eb1bb96157de83e871e5b8c4ebd
Tony Doan <tony.thinh.doan@gmail.com>
2020-02-20 19:40:58 -0800

    Added ESLint configurations, Prettier configurations, package.json configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-const App = <div>
-    <h1>Adopt Me! hey!</h1>
-</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-ReactDOM.render(App, document.getElementById("root"));
+  render() {
+    return <Div name={1} />;
+  }
+}
 
+class Div extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          My name is {this.props.name}. I am {this.props.age} years old.
+        </h1>
+      </div>
+    );
+  }
+}
+
+Div.defaultProps = {
+  name: "Nameless",
+  age: 0
+};
+
+Div.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired
+};
+
+let namesList = ["Bob", "Jane", "Rick"];
+
+ReactDOM.render(
+  <App
+    name={namesList}
+    age={[12, 13, 14]}
+    hash={{ country: "U.S.", state: "Nevada" }}
+  />,
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-// Webpack uses this to wrok with directories
+// Webpack uses this to work with directories
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // This is the main configuration object.
 // Here, you write different options and tell Webpack what to do.
 module.exports = {
-    // Pat to your entry point. From this file, Webpack will begin its work.
+    // Path to your entry point. From this file, Webpack will begin its work.
     entry: "./src/app.js",
 
-    // Path and filename of your result bundle.
-    // Webpack will bundle all Javascript into this file.
+    // Webpack will bundle all Javascript source code into <filename> and output 
+    // to <path>. FYI, __dirname is the 
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
 
     plugins: [
+        // Automatically creates an HTML file to add your bundled JS file via
+        // the <script> tag. If a template is provided, then it will a <script>
+        // tag into the template instead. By default, the generated HTML file 
+        // will be exported to output.path (which is defined above).
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
     ],
 
+    // Deploys a server to automatically (by default) load your HTML file.
+    // Reduces the development process by not requiring you to manually
+    // load your HTML file into your browser.
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         port: 8080
